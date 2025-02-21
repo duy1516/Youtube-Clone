8e4921cc4dd53bcbe9b471375a11fb843d9ed142
duy1516 <duynguyenhoang1611@gmail.com>
2025-02-21 22:12:18 +0700

    trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
+import { HydrateClient, trpc } from "@/trpc/server"
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
+export default async function Home() {
+    void trpc.hello.prefetch({ text: "Duy" });
 
-export default function Home() {
     return (
-        <div>
-            videos
-        </div>
+        <HydrateClient>
+            <Suspense fallback={<p>Loading...</p>}>
+                <ErrorBoundary fallback={<p>Error...</p>}>
+                    <PageClient />
+                </ErrorBoundary>
+            </Suspense>
+        </HydrateClient>
     )
 }
