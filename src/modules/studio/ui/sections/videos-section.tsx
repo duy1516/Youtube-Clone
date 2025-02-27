f01a00db53dbe3938d4ac5399abc8ed121dd04cf
duy1516 <duynguyenhoang1611@gmail.com>
2025-02-27 16:28:51 +0700

    Mux integration and webhooks

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,16 +1,106 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideosSection = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error</p>}>
+                <VideosSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+
+const VideosSectionSuspense = () => {
+    const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT,
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
     });
 
     return (
-        <div>Videos section</div>
+        <div>
+            <div className=" border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">Comments</TableHead>
+                            <TableHead className="text-right pr-6">Likes</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {videos.pages.flatMap((page) => page.items).map((video) => (
+                            <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+                                <TableRow className="cursor-pointer select-none">
+                                    <TableCell>
+                                        <div className="flex items-center gap-4">
+                                            <div className="relative aspect-video w-36 shrink-0">
+                                                <VideoThumbnail
+                                                    imageUrl={video.thumbnailUrl}
+                                                    previewUrl={video.previewUrl}
+                                                    title={video.title}
+                                                    duration={video.duration || 0}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col overflow-hidden gap-y-1">
+                                                <span className="text-sm line-clamp-1">{video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1">{video.description || "No description"}</span>
+                                            </div>
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center">
+                                            {video.visibility === "private" ? (
+                                                <LockIcon className="size-4 mr-2" />
+                                            ) : (
+                                                <Globe2Icon className="size-4 mr-2" />
+                                            )}
+                                            {snakeCaseToTitle(video.visibility)}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center">
+                                            {snakeCaseToTitle(video.muxStatus || "error")}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell className="text-sm truncate">
+                                        {format(new Date(video.createdAt), "d MMM yyyy")}
+                                    </TableCell>
+                                    <TableCell>
+                                        Views
+                                    </TableCell>
+                                    <TableCell>
+                                        Comments
+                                    </TableCell>
+                                    <TableCell>
+                                        Likes
+                                    </TableCell>
+                                </TableRow>
+                            </Link>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            <InfiniteScroll
+                hasNextPage={query.hasNextPage}
+                isFetchingNextPage={query.isFetchingNextPage}
+                fetchNextPage={query.fetchNextPage} />
+        </div>
     );
 };
--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -2,21 +2,43 @@ import { z } from "zod";
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { mux } from "@/lib/mux";
 
 export const videosRouter = createTRPCRouter({
     create: protectedProcedure.mutation(async ({ ctx }) => {
         const { id: userId } = ctx.user;
+
+        const upload = await mux.video.uploads.create({
+            new_asset_settings: {
+                passthrough: userId,
+                playback_policy: ["public"],
+                input: [
+                    {
+                        generated_subtitles: [
+                            {
+                                language_code: "en",
+                                name: "English",
+                            },
+                        ],
+                    },
+                ],
+            },
+            cors_origin: "*", //TODO: in production, set to your url
+        });
 
         const [video] = await db
             .insert(videos)
             .values({
                 userId,
                 title: "Untitled",
+                muxStatus: "waiting",
+                muxUploadId: upload.id,
             })
             .returning();
 
         return {
             video: video,
+            url: upload.url,
         };
     }),
 });
